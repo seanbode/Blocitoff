610f24bf8099fab149b65ba1d59e0af28397201f
Sean Bode <seanpaulbode@gmail.com>
2016-07-02 18:57:58 -0400

    CRUD delete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,28 +4,55 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = current_user.items.build(params.require(:item).permit(:name))
+    @item = current_user.items.build(post_params)
 
-    # The action should create a new item associated with a user.
     if @item.save
       flash[:notice] = "Item saved successfully."
-      redirect_to items_path #redirects out of form to completed view.
+      redirect_to items_path
     else
       flash[:alert] = "Item failed to save."
-      render :new # clears the form to start over.
+      render :new
     end
-
-    #the following is ajax scripting. Incorporates Javascript & Html in formatting?
-    #respond_to do |format|
-    #  format.html
-    #  format.js
-    #end
   end
 
   def new
     @item = Item.new
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
   def show
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.assign_attributes(post_params)
+
+    if @item.save
+      flash[:notice] = "Item saved successfully."
+      redirect_to items_path
+    else
+      flash[:alert] = "Item failed to save."
+      render :new
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
+      flash[:notice] = "Item was deleted successfully."
+      redirect_to item_path
+    else
+      flash[:now] = "There was an error deleting the post."
+      render :show
+    end
+  end
+
+private
+
+  def post_params
+    params.require(:item).permit(:name)
   end
 end
